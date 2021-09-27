4694e0a3627ca92656f6fc0739d4d1404c1c9e7a
anilmertergul <78846973+anilmertergul@users.noreply.github.com>
2021-09-27 09:11:47 +0300

    Add files via upload

---
+++ dvuichni/dvuichni/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvuichni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string des = Console.ReadLine();
+            int bin = Convert.ToInt32(des, 2);   
+            int dec = Convert.ToInt32(x, 16);
+            Console.WriteLine("dvuichno chislo "+bin);
+            Console.WriteLine("shesn chislo "+dec);
+
+
+        }
+    }
+}
---
+++ dvuichni/dvvshestd/dvvshestd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvvshestd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            int dec = Convert.ToInt32(x, 16);
+            Console.WriteLine(dec);
+        }
+    }
+}
